8c9e4af4648a2d5c4e9dbee843c180f6324f40d4
Amina Said Ibrahim <amina7585@gmail.com>
2025-05-17 00:40:28 +0100

    feat(footer): implemented GitHub and LinkedIn icons with accessibility and usability attributes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
+import "../App.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 export default function Footer() {
   return (
     <>
       <footer>
-        <h1>Footer here ...</h1>
+        <a
+          href="https://github.com/Amishka-Sayyid"
+          aria-label="GitHub profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+        <span> </span>
+        <a
+          href="https://www.linkedin.com/in/amina-ibrahim-78944623b/"
+          aria-label="linkedin profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
+        <p> © 2025. All Rights Reserved.</p>
       </footer>
     </>
   );
