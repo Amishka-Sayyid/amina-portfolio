6a93512926372b65fd32b890202b4afce8848e02
Amina Said Ibrahim <amina7585@gmail.com>
2025-05-22 23:06:06 +0100

    saved contact form to database and send email notification

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import pg from "pg";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 
 //start or configure packages
 const app = express();
@@ -41,6 +42,8 @@ app.post("/contact", async (req, res) => {
   // console.log(req.body);
 
   const { name, email, message } = req.body;
+
+  // 1. Save to DB
   try {
     await db.query(
       "INSERT INTO portfolio (name, email, message) VALUES ($1, $2, $3)",
@@ -50,5 +53,29 @@ app.post("/contact", async (req, res) => {
   } catch (error) {
     console.error("DB insert error:", error);
     res.status(500).send({ error: "Failed to save message" });
+  }
+
+  // 2. Send email
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"Portfolio Contact Form" <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_USER,
+    subject: "New Portfolio Contact Form Submission",
+    text: `You got a message from ${name} (${email}):\n\n\n\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send({ success: true });
+  } catch (error) {
+    console.error("Email sending error:", error);
+    res.status(500).send({ error: "Failed to send email" });
   }
 });
